fa9d895d28d39018e10015679c3eb414721a11a5
Gaus Saraf Murady <63799660+b1tranger@users.noreply.github.com>
2025-05-25 23:09:14 +0600

    added a new game

--- Raylib-Game-Project/Ping-Pong-Game/Ping-Pong-Game/Ping-Pong-Game.cpp
+++ Raylib-Game-Project/Ping-Pong-Game/Ping-Pong-Game/Ping-Pong-Game.cpp
@@ -1,15 +1,22 @@
-#include <iostream>
 #include <raylib.h>
-using namespace std;
+#include <iostream>
+
+Color Green = Color{ 38, 185, 154, 255 };
+Color Dark_Green = Color{ 20, 160, 133, 255 };
+Color Light_Green = Color{ 129, 204, 184, 255 };
+Color Yellow = Color{ 243, 213, 91, 255 };
+
+int player_score = 0;
+int cpu_score = 0;
 
 class Ball {
 public:
-    float  x, y;
+    float x, y;
     int speed_x, speed_y;
     int radius;
 
     void Draw() {
-        DrawCircle(x, y, radius, WHITE);
+        DrawCircle(x, y, radius, Yellow);
     }
 
     void Update() {
@@ -19,12 +26,26 @@ public:
         if (y + radius >= GetScreenHeight() || y - radius <= 0) {
             speed_y *= -1;
         }
-        if (x + radius >= GetScreenWidth() || x - radius <= 0) {
-            speed_x *= -1;
+        // Cpu wins
+        if (x + radius >= GetScreenWidth()) {
+            cpu_score++;
+            ResetBall();
         }
 
+        if (x - radius <= 0) {
+            player_score++;
+            ResetBall();
+        }
     }
 
+    void ResetBall() {
+        x = GetScreenWidth() / 2;
+        y = GetScreenHeight() / 2;
+
+        int speed_choices[2] = { -1, 1 };
+        speed_x *= speed_choices[GetRandomValue(0, 1)];
+        speed_y *= speed_choices[GetRandomValue(0, 1)];
+    }
 };
 
 class Paddle {
@@ -58,20 +79,29 @@ public:
     }
 };
 
+class CpuPaddle : public Paddle {
+public:
+    void Update(int ball_y) {
+        if (y + height / 2 > ball_y) {
+            y = y - speed;
+        }
+        if (y + height / 2 <= ball_y) {
+            y = y + speed;
+        }
+        LimitMovement();
+    }
+};
 
+Ball ball;
+Paddle player;
+CpuPaddle cpu;
 
-Ball ball;    // Ball instances
-Paddle player; //Paddle instance
-
-int main()
-{
-    cout << "starting the game" << endl;
+int main() {
+    std::cout << "Starting the game" << std::endl;
     const int screen_width = 1280;
     const int screen_height = 800;
-    InitWindow(screen_width, screen_height, "my Pong Game!");
+    InitWindow(screen_width, screen_height, "My Pong Game!");
     SetTargetFPS(60);
-
-
     ball.radius = 20;
     ball.x = screen_width / 2;
     ball.y = screen_height / 2;
@@ -84,36 +114,44 @@ int main()
     player.y = screen_height / 2 - player.height / 2;
     player.speed = 6;
 
-    //Game Loop
-    while (WindowShouldClose() == false) {
+    cpu.height = 120;
+    cpu.width = 25;
+    cpu.x = 10;
+    cpu.y = screen_height / 2 - cpu.height / 2;
+    cpu.speed = 6;
 
+    while (WindowShouldClose() == false) {
         BeginDrawing();
 
+        // Updating
 
-        //updating
         ball.Update();
         player.Update();
+        cpu.Update(ball.y);
 
-        
+        // Checking for collisions
+        if (CheckCollisionCircleRec({ ball.x, ball.y }, ball.radius, { player.x, player.y, player.width, player.height })) {
+            ball.speed_x *= -1;
+        }
+
+        if (CheckCollisionCircleRec({ ball.x, ball.y }, ball.radius, { cpu.x, cpu.y, cpu.width, cpu.height })) {
+            ball.speed_x *= -1;
+        }
 
         // Drawing
-        ClearBackground(BLACK);
+        ClearBackground(Dark_Green);
+        DrawRectangle(screen_width / 2, 0, screen_width / 2, screen_height, Green);
+        DrawCircle(screen_width / 2, screen_height / 2, 150, Light_Green);
         DrawLine(screen_width / 2, 0, screen_width / 2, screen_height, WHITE);
         ball.Draw();
-        
-        //DrawCircle(screen_width/2, screen_height/2, 20, WHITE);
-        DrawRectangle(10, screen_height / 2 - 60, 25, 120, WHITE);
-        //DrawRectangle(screen_width - 25 - 10, screen_height / 2 - 60, 25, 120, WHITE);
+        cpu.Draw();
         player.Draw();
+        DrawText(TextFormat("%i", cpu_score), screen_width / 4 - 20, 20, 80, WHITE);
+        DrawText(TextFormat("%i", player_score), 3 * screen_width / 4 - 20, 20, 80, WHITE);
 
         EndDrawing();
     }
 
-
-
     CloseWindow();
-
-
-
     return 0;
 }
